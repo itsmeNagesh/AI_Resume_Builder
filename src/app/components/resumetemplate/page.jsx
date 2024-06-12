6b5d7339fe6550f31ab96c126477bb32a48c8617
itsNagesh <nagesh91555@gmail.com>
2024-06-12 18:13:27 +0530

    &apos

--- src/app/components/resumetemplate/page.jsx
+++ src/app/components/resumetemplate/page.jsx
@@ -154,13 +154,15 @@ console.log('skillre', addSkill);
                 <div className="section__list-item">
                   <div className="name">DSP</div>
                   <div className="text">
-                    I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.
+                    I am a front-end developer with more than 3 years of experience writing html, css,
+                     and js. I&apos;m motivated, result-focused and seeking a successful team-oriented company with opportunity to grow.
                   </div>
                 </div>
                 <div className="section__list-item">
                   <div className="name">DSP</div>
                   <div className="text">
-                    I am a front-end developer with more than 3 years of experience writing html, css, and js. I'm motivated, result-focused and seeking a successful team-oriented company with opportunity to grow. <a href="/login">link</a>
+                    I am a front-end developer with more than 3 years of experience writing html, css, and 
+                    js. I&apos;m motivated, result-focused and seeking a successful team-oriented company with opportunity to grow. <a href="/login">link</a>
                   </div>
                 </div>
               </div>
